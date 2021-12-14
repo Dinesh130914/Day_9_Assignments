b616aacc1071d9288a1fa266a92e3af6b5e0a139
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-12-14 15:07:22 +0530

    Added :Editing Details

---
+++ src/com/bl/addressbook/EditingdetailsUC3.java
@@ -0,0 +1,53 @@
+package com.bl.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EditingdetailsUC3 {
+	public static void add(String fName, String lName, String address,  String state, String zipcode, String contcnumber, String email) 
+	{
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add(fName);
+		list.add(lName);
+		list.add(address);
+		list.add(state);
+		list.add(zipcode);
+		list.add(contcnumber);
+		list.add(email);
+		
+		for (int i = 0; i < 1; i++) {
+			System.out.println("Your Address Book :"+list);
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the Latest Firstname");
+			String fname = sc.next();
+			list.set(0, fname);
+			System.out.println("Successfully Edited");
+			
+		}
+		System.out.println("Edited Address Book "+list);
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the first name");
+		String fName = sc.next();
+		System.out.println("Enter the lastename");
+		String lName  = sc.next();
+		System.out.println("Enter the address");
+		String address = sc.next();
+		System.out.println("Enter the state");
+		String state = sc.next();
+		System.out.println("Enter the zipcode");
+		String zipcode  = sc.next();
+		System.out.println("Enter the contact number");
+		String contctnumber = sc.next();
+		System.out.println("Enter the mail id");
+		String email = sc.next();
+		
+		add(fName, lName, address, state, zipcode, contctnumber, email);
+	}
+}
